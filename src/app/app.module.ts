65c98c55a04d00a912cbdc48b601a80b0d0a3845
Andras Kovacs <Andras.Kovacs4@ge.com>
2018-01-11 15:37:56 +0100

    adding kit module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { KitModule } from './kit/kit.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KitModule
   ],
   providers: [],
   bootstrap: [AppComponent]
